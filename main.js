d6994cfd9cf2e7aa55c9047e8da84c58218a6852
zhangzju <zhangway19921221@163.com>
2016-09-18 02:41:20 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow} = require('electron')
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({width: 800, height: 600})
+
+  // and load the index.html of the app.
+  win.loadURL(`file://${__dirname}/index.html`)
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
